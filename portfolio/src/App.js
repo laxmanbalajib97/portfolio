98001dcfacc95fb2a0b410b93b45f8e06be0cb38
Laxman Balaji Balasubramanian <laxmanbalajib@tamu.edu>
2020-06-19 03:08:00 -0500

    hello from the app

---
+++ portfolio/src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+class App extends React.Component {
+  render() {
+    return <div> Hello from the app </div>;
+  }
+}
+
+export default App;
