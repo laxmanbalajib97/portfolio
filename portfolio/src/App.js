854e8146fe6f03fd3e742c9bf96d2fa64f295199
Laxman Balaji Balasubramanian <laxmanbalajib@tamu.edu>
2020-06-20 05:50:01 -0500

    fixed navbar width issue and changed bg color

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -35,62 +35,64 @@ class App extends React.Component {
   }
   render() {
     return (
-      <Router>
-        {" "}
-        <Container className="p0" fluid={true}>
-          <Navbar className="border-bottom " bg="transparent" expand="lg">
-            <Navbar.Brand>Balasubramanian, Laxman Balaji</Navbar.Brand>
-            <Navbar.Toggle aria-controls="navbar-toggle" />
-            <Navbar.Collapse id="navbar-toggle">
-              {/*ml-auto = margin left auto class*/}
-              <Nav className="ml-auto">
-                <Link className="nav-link" to="/">
-                  Home
-                </Link>
-                <Link className="nav-link" to="/about">
-                  About
-                </Link>
-                <Link className="nav-link" to="/contact">
-                  Contact
-                </Link>
-              </Nav>
-            </Navbar.Collapse>
-          </Navbar>
-          <Route
-            path="/"
-            exact
-            render={() => (
-              <HomePage
-                title={this.state.home.title}
-                text={this.state.home.text}
-              />
-            )}
-          />
-          <Route
-            path="/about"
-            exact
-            render={() => (
-              <AboutPage
-                title={this.state.home.title}
-                subtitle="this.state.home.subtitle"
-                text="this.state.home.text"
-              />
-            )}
-          />
-          <Route
-            path="/contact"
-            exact
-            render={() => (
-              <ContactPage
-                title={this.state.home.title}
-                subtitle="this.state.home.subtitle"
-                text="this.state.home.text"
-              />
-            )}
-          />
-          <Footer />
-        </Container>
-      </Router>
+      <div>
+        <Router>
+          {" "}
+          <Container fluid={true} className="p-0">
+            <Navbar expand="lg" bg="dark" variant="dark">
+              <Navbar.Brand>Balasubramanian, Laxman Balaji</Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbar-toggle" />
+              <Navbar.Collapse id="navbar-toggle">
+                {/*ml-auto = margin left auto class*/}
+                <Nav className="ml-auto">
+                  <Link className="nav-link" to="/">
+                    Home
+                  </Link>
+                  <Link className="nav-link" to="/about">
+                    About
+                  </Link>
+                  <Link className="nav-link" to="/contact">
+                    Contact
+                  </Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Navbar>
+            <Route
+              path="/"
+              exact
+              render={() => (
+                <HomePage
+                  title={this.state.home.title}
+                  text={this.state.home.text}
+                />
+              )}
+            />
+            <Route
+              path="/about"
+              exact
+              render={() => (
+                <AboutPage
+                  title={this.state.home.title}
+                  subtitle="this.state.home.subtitle"
+                  text="this.state.home.text"
+                />
+              )}
+            />
+            <Route
+              path="/contact"
+              exact
+              render={() => (
+                <ContactPage
+                  title={this.state.home.title}
+                  subtitle="this.state.home.subtitle"
+                  text="this.state.home.text"
+                />
+              )}
+            />
+            <Footer />
+          </Container>
+        </Router>
+      </div>
     );
   }
 }
